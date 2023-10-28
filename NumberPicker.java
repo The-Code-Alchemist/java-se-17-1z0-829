53b926167b9ab88f6ce1c21377e3e58e2d88e1fb
Onur Akgün <onur.akgun@code-alchemists.com>
2023-10-28 22:02:22 +0200

    NumberPicker.java from page 11-12 Import and use java.util.Random

--- NumberPicker.java
+++ NumberPicker.java
@@ -4,5 +4,5 @@ public class NumberPicker {
     public static void main(String[] args) {
         Random r = new Random();
         System.out.println(r.nextInt(10)); // will not compile without the import on line 1
-    }   // from page 11
+    }   // from page 11-12
 }
