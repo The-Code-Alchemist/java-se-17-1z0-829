d7d34bfbd0998da6ad636657e7f64088c41bd329
Onur Akgün <onur.akgun@code-alchemists.com>
2023-10-29 00:33:44 +0200

    Conflicts.java only 1 import can be a wildcard, not both fully qualified name allows both classes to be used

--- Conflicts.java
+++ Conflicts.java
@@ -2,6 +2,7 @@ import java.sql.Date;
 import java.util.*;
 
 public class Conflicts {
-    Date date; // java.sql.Date;
+    Date date; // java.sql.Date; // Explicit imports take precedence over wildcard imports.
+    java.util.Date utilDate = new java.util.Date(); // can be used with the fully qualified name, though
     // page 15
 }
