db1c7d9a1ea71e9f29307863313d5bcb6110dec2
Onur Akgün <onur.akgun@code-alchemists.com>
2023-10-28 21:44:59 +0200

    Zoo.java from page 10 How to use a sentence as an argument

--- Zoo.java
+++ Zoo.java
@@ -3,7 +3,7 @@ public class Zoo {
         System.out.println(args[0]);
         System.out.println(args[1]);
     }
-}   // page 9-10 // java Zoo.java Bronx Zoo
+}   // page 9-10 // java Zoo.java "Bronx is a borough in New York City" Zoo
 
 // passing only one argument causes an ArrayIndexOutOfBoundsException on line 4
 // passing only no arguments causes an ArrayIndexOutOfBoundsException on line 3
