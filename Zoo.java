ed71a9e580a35919738a1f92ede7ec5f5e536ab8
Onur Akgün <onur.akgun@code-alchemists.com>
2023-10-28 21:36:21 +0200

    Zoo.java from page 9-10 single command to compile and run Refer tot the comment

--- Zoo.java
+++ Zoo.java
@@ -1,5 +1,6 @@
 public class Zoo {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
-}   // page 8
+}   // page 9-10 // java Zoo.java Bronx Zoo
