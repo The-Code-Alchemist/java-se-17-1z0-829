01217be79a8fb936e5b7f6fa87c604757e791f41
Onur Akgün <onur.akgun@code-alchemists.com>
2023-10-29 18:30:33 +0100

    PreAndPostfix.java demonstrates prefix and postfix operations

---
+++ PreAndPostfix.java
@@ -0,0 +1,11 @@
+public class PreAndPostfix {
+    public static void main(String[] args) {
+        int parkAtendance = 0;
+
+        System.out.println(parkAtendance);      // 0
+        System.out.println(++parkAtendance);    // 1
+        System.out.println(parkAtendance);      // 1
+        System.out.println(parkAtendance--);    // 1
+        System.out.println(parkAtendance);      // 0
+    }   // pages 71 and 72
+}
