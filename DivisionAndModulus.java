dc7a76fcda5126c3d8b5da78ba35d4ad0ba751ae
Onur Akgün <onur.akgun@code-alchemists.com>
2023-10-29 19:10:03 +0100

    DivisionAndModulus.java page 74 and 75

---
+++ DivisionAndModulus.java
@@ -0,0 +1,15 @@
+public class DivisionAndModulus {
+    public static void main(String[] args) {
+        System.out.println(9 / 3);  //  3
+        System.out.println(9 % 3);  //  0
+
+        System.out.println(10 / 3); //  3
+        System.out.println(10 % 3); //  1
+
+        System.out.println(11 / 3); //  3
+        System.out.println(11 % 3); //  2
+
+        System.out.println(12 / 3); //  4
+        System.out.println(12 % 3); //  0
+    }   //  page 74 and 75
+}
