57abe0f3c696f70dfada79575810eb7672f9d6e4
Onur Akgün <onur.akgun@code-alchemists.com>
2023-10-29 18:49:50 +0100

    BinaryArithmeticOperations.java gives an example of operator precedence

---
+++ BinaryArithmeticOperations.java
@@ -0,0 +1,10 @@
+public class BinaryArithmeticOperations {
+    public static void main(String[] args) {
+        int price = 2 * 5 + 3 * 4 - 8;  //  10 + 12 - 8 = 14
+        System.out.println(price);      //  14
+
+        price = 2 * ((5 + 3) * 4 - 8);  // 2 * (8 * 4 - 8) = 2 * (32 - 8) = 2 * 24 = 48
+        System.out.println(price);
+
+    }   // page 73
+}
