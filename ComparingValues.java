0ccc172e0f9eaf521c3403aac184f146582ca5e3
Onur Akgün <onur.akgun@code-alchemists.com>
2023-10-30 13:26:57 +0100

    ComparingValues.java comparing some int values

---
+++ ComparingValues.java
@@ -0,0 +1,9 @@
+public class ComparingValues {
+    public static void main(String[] args) {
+        int gibbonNumFeet = 2, wolfNumFeet = 4, ostrichNumFeet = 2;
+        System.out.println(gibbonNumFeet < wolfNumFeet);
+        System.out.println(gibbonNumFeet <= wolfNumFeet);
+        System.out.println(gibbonNumFeet >= ostrichNumFeet);
+        System.out.println(gibbonNumFeet > ostrichNumFeet);
+    }   //  from page 85
+}
