88b0265ebcfdb1637d63a143186e360f39c8cf08
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-11-17 11:32:14 +0100

    Comment updated

--- Zoo.java
+++ Zoo.java
@@ -3,7 +3,7 @@
  * Passing only no arguments causes an ArrayIndexOutOfBoundsException on line 13
  * Passing only one argument causes an ArrayIndexOutOfBoundsException on line 14
  *
- * Example and explanation: the next line compiles and runs Zoo.java with one command
+ * Explanation: the next line compiles and runs Zoo.java with one command which can be used on single sources
  * java Zoo.java "Bronx is a borough in New York City" Zoo
  * The text between double quotes is a single argument
  * A space separates arguments
