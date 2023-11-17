e1f7a22cb50b5739532a2282f78409921f2ca63d
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-11-17 19:53:45 +0100

    Comment updated

--- tmp/packageb/ClassB.java
+++ tmp/packageb/ClassB.java
@@ -1,6 +1,13 @@
 package tmp.packageb;
 import tmp.*;
 import tmp.packagea.ClassA;
+
+/**
+ * @author The Code Alchemist
+ * ClassC can be imported using a wildcard import such as on line 2 or explicitly
+ * import tmp.ClassC;
+ *
+ */
 public class ClassB {
     public static void main(String[] args) {
         ClassA a;
