9da862095dbd53505c4c23f6fb3151637ce072c6
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-11-17 16:54:45 +0100

    Comment and import updated

--- NumberPicker.java
+++ NumberPicker.java
@@ -1,9 +1,16 @@
-import java.util.*; // wildcard import
+import java.util.Random;
 
+/**
+ * @author The Code Alchemist
+ * The class java.util.Random needs to be imported which cn be achieved in three ways
+ * 1 wildcard   import java.util.*;
+ * 2 explicitly import java.util.Random
+ * 3 using the fully qualified name the same way on line 14
+ */
 public class NumberPicker {
     public static void main(String[] args) {
         Random r = new Random();
-        System.out.println(r.nextInt(10)); // wildcard imported
-        System.out.println(new Date());          // wildcard imported
-    }   // from page 13
+        System.out.println(r.nextInt(10));
+        System.out.println(new java.util.Date());
+    }   // from page 11-13
 }
