a11b1bcbce0b7d6dfcfd2e80a0e11481772ad8a7
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-11-17 19:17:58 +0100

    Comment updated

--- Conflicts.java
+++ Conflicts.java
@@ -1,8 +1,25 @@
 import java.sql.Date;
 import java.util.*;
 
+/**
+ * @author The Code Alchemist
+ * Explicit imports take precedence over wildcard imports.
+ * Using both java.util and java.util with a wildcard will cause a compiler error
+ *
+ */
 public class Conflicts {
-    Date date; // java.sql.Date; // Explicit imports take precedence over wildcard imports.
-    java.util.Date utilDate = new java.util.Date(); // can be used with the fully qualified name, though
-    // page 15
-}
+
+    public static void main(String[] args) {
+        /**
+         * java.sql.Date is used here
+         */
+        Date sqlDate = new Date(2023,11,17);
+        System.out.println(sqlDate);
+
+        /**
+         * java.util.Date can be used with the fully qualified name without importing it
+         */
+        java.util.Date utilDate = new java.util.Date();
+        System.out.println(utilDate);
+    }
+}   // page 15
