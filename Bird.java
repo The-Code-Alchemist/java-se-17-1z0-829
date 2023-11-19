925529dbfb765a3510f3c933c9790efbd035ddd1
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-11-19 17:40:48 +0100

    Code blocks explained

--- Bird.java
+++ Bird.java
@@ -1,6 +1,14 @@
-public class Bird {
-    public static void main(String[] args) {
-        { System.out.println("Feathers"); }
+/**
+ * @author The Code Alchemist
+ * There are four blocks in this example
+ * 1) class definition
+ * 2) main method declaration
+ * 3) inner block within the main method
+ * 4) instance initializer
+ */
+public class Bird { // 1
+    public static void main(String[] args) { // 2
+        { System.out.println("Feathers"); /*3*/ }
     }
-    { System.out.println("Snowy"); }
-}
+    { System.out.println("Snowy"); } // 4
+}   // page 25
