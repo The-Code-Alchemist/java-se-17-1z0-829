6942b3a5386c13bdad912841631bd3222e4a2eb8
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-11-19 16:57:41 +0100

    Constructor example

---
+++ Chick.java
@@ -0,0 +1,17 @@
+/**
+ * @author The Code Alchemist
+ * The constructor of a class mathces the class name and has no return type, not even void
+ */
+public class Chick {
+
+    static String name = "Chickie";
+
+    public Chick() {
+        System.out.println("The default constructor has been called");
+    }
+
+    public static void main(String[] args) {
+        Chick chick = new Chick();  // although the reference variable is not used further, the constructor is called
+        System.out.println("Hi " + name);
+    }
+}
