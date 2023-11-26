2d3c0927307f84d69f5b6b2969f21b4c8433739b
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-11-26 18:43:05 +0100

    non-static example added

--- Swan.java
+++ Swan.java
@@ -3,6 +3,7 @@ public class Swan {
     public static void main(String[] args) {
         Swan mother = new Swan();
         mother.numberEggs = 1;                  // set variable
+        // numberEggs = 7;                      // Non-static field 'numberEggs' cannot be referenced from a static context
         System.out.println(mother.numberEggs);  // read variable
     }   // page 24
 }
