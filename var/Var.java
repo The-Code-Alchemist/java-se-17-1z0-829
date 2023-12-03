0f9fa142c25624423cf187c1364b22adece6e445
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-12-03 19:06:19 +0100

    VarKeyword example and explanation updated

---
+++ var/Var.java
@@ -0,0 +1,12 @@
+/**
+ * @author The Code Alchemist
+ * var is not a reserved word. var can be used as a variable name.
+ */
+package var;
+public class Var {
+    public static void main(String[] args) {
+        Var var = new Var();
+
+//        var var = "Var";
+    }
+}
