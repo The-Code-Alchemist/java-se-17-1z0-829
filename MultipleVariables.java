7bcfaeea0e9202cf986f79f24ac09bbe60d185a3
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-12-03 15:51:33 +0100

    MultipleVariables on one line

---
+++ MultipleVariables.java
@@ -0,0 +1,12 @@
+/**
+ * @author The Code Alchemist
+ * multiple variables can be declared on a single line if they are of the same type
+ */
+public class MultipleVariables {
+    public static void main(String[] args) {
+        String s1, s2;
+        String s3 = "yes", s4 = "no";
+
+        int i1, i2, i3, i4 = 0;
+    }   // pages 36-37
+}
