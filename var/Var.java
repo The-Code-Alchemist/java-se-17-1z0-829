f4717f62f77da8270e98966bda2de6092259457a
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-12-03 19:09:27 +0100

    Var explanation

--- var/Var.java
+++ var/Var.java
@@ -1,6 +1,8 @@
 /**
  * @author The Code Alchemist
  * var is not a reserved word. var can be used as a variable name.
+ * guidelines can be found here
+ * https://openjdk.org/projects/amber/guides/lvti-style-guide
  */
 package var;
 public class Var {
