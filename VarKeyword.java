a3d5e742f6f4c7c218d2fb2b5c09b6c5aa38a891
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-12-03 18:57:09 +0100

    VarKeyword example and explanation updated

--- VarKeyword.java
+++ VarKeyword.java
@@ -2,13 +2,16 @@
  * @author The Code Alchemist
  * We are instructing the compiler to determine the variable's type on line 7 which is an int
  * A String value can't be assigned to a variable that was determined to be an int already such as on line 10
- * line 10 does int number = "five";
+ * line 11 tries int number = "five";
+ * line 12 shows that null can't be assigned to var
  */
 public class VarKeyword {
     public static void main(String[] args) {
         var number = 7;
         number = 4;
-//        var number = "five";    // Can't assign String value to an int variable.
+//        var number = "five";      // Can't assign String value to an int variable.
+
+//        var cannotBeNull = null;  //
     }   // page 42
 
 }
