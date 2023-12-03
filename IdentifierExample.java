835d43ffa96336ba63dc07b7abf15c17da7178df
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-12-03 15:50:59 +0100

    IdentifierExample for demo

---
+++ IdentifierExample.java
@@ -0,0 +1,15 @@
+public class IdentifierExample {
+    public static void main(String[] args) {
+        long okIdentifier;
+        float $OK2identifier;
+        boolean _alsoOK1d3ntifi3r;
+        char __SStillOkbutKnotsonice$;
+
+//        will not compile
+//        int 3DPointClass;
+//        byte hollywood@vine;
+//        String *$coffee;
+//        double public;
+//        short _;
+    }   // page 36
+}
