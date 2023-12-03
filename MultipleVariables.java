be49c8e05b292b8f1272e1b80266ce23902df480
The Code Alchemist <onur.akgun@code-alchemists.com>
2023-12-03 16:10:28 +0100

    MultipleVariables on one line, additional examples

--- MultipleVariables.java
+++ MultipleVariables.java
@@ -8,5 +8,14 @@ public class MultipleVariables {
         String s3 = "yes", s4 = "no";
 
         int i1, i2, i3, i4 = 0;
+
+        boolean b1, b2;
+
+        String s5 = "1", s6;
+
+//        double d1, double d2;   // will not compile
+
+        int i5; int i6;
+//        int i7; i8;             // will not compile
     }   // pages 36-37
 }
